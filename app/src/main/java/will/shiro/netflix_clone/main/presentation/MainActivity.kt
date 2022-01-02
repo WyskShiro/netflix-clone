130fd578fdbf7e18e601f9080e0dce0ec767a309
Wyskshiro <wyskanashiro@gmail.com>
2022-01-02 15:20:51 -0400

    Adding alpha for toolbar

--- app/src/main/java/will/shiro/netflix_clone/main/presentation/MainActivity.kt
+++ app/src/main/java/will/shiro/netflix_clone/main/presentation/MainActivity.kt
@@ -1,13 +1,18 @@
 package will.shiro.netflix_clone.main.presentation
 
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
+import android.util.DisplayMetrics
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import dagger.android.AndroidInjection
 import will.shiro.netflix_clone.databinding.ActivityMainBinding
 import javax.inject.Inject
+import kotlin.math.min
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,6 +24,11 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private val adapter by lazy {
         FeedAdapter()
+    }
+    private val featuredImageWidth by lazy {
+        val displayMetrics = DisplayMetrics()
+        windowManager.defaultDisplay.getMetrics(displayMetrics)
+        displayMetrics.widthPixels
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -34,11 +44,23 @@ class MainActivity : AppCompatActivity() {
     private fun setUpUI() = binding.run {
         feedRecycler.layoutManager = LinearLayoutManager(this@MainActivity)
         feedRecycler.adapter = adapter
+        feedRecycler.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                binding.appBarLayout.background = ColorDrawable(Color.BLACK).apply { alpha = getAlphaForAppBarLayout() }
+            }
+        })
     }
 
     private fun setUpObservers() {
         viewModel.feedItems.observe(this) {
             adapter.submitList(it)
         }
+    }
+
+    private fun getAlphaForAppBarLayout(): Int {
+        return min(
+            255f,
+            (binding.feedRecycler.computeVerticalScrollOffset().toFloat() / featuredImageWidth.toFloat()) * 255
+        ).toInt()
     }
 }
